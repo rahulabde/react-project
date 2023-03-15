4a949f78b95321703abcbb8aaa67315cb76d7df5
Rahul <rahulabde@gmail.com>
2023-03-15 22:56:31 +0530

    state updating

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,37 @@
+
 import React, { Component } from 'react'
-import Json from "./user.json"
-import Users from "./Users.jsx"
 
 export default class App extends Component {
-  constructor(){
-    super()
-    this.state={json:Json}
-  }
-  render() {
+  
+    constructor()
+    {
+        super()
+        this.state={
+            username:"sam",
+            count:0
+        }
+    }
+    
+    handleChange= ()=>{
+        this.setState({
+            username:"suje"
+            
+        })
+        console.log(this);
+    }
+    handleNumberChange=()=>{
+        this.setState({
+            count:1
+        })
+    }
+    render() {
     return (
       <div>
-        <Users data={this.state.json}/>
+        <h1 > {this.state.username}</h1>
+        <h1>{this.state.count}</h1>
+        <button onClick={this.handleChange}>click to change</button>
+        <button onClick={this.handleNumberChange}>change the number</button>
       </div>
     )
-  }
+    }
 }

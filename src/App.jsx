566cb59a6f547a345af2fd619757182139113419
Rahul <rahulabde@gmail.com>
2023-03-17 22:04:44 +0530

    Toast

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,20 @@
 import React from 'react'
-import "./global1.css"
+import { toast, ToastContainer } from 'react-toastify'
+import "react-toastify/dist/ReactToastify.css"
 
-const App=()=>{
-  let[count,setcount]=React.useState(0)
-  let handleincrement=()=>{
-    setcount(count+1)
+const App = () => {
+
+  let handlesubmit=()=>{
+    toast.success("logged in successfully",{position:toast.POSITION.TOP_CENTER})
   }
-  let handledecrement=()=>{
-    setcount(count-1)
-  }
-  let handlereset=()=>{
-    setcount(0)
-  }
-  return(
-    <> <div className='main'>
-   <center> <h1>{count}</h1></center>
-   </div>
-   <div className='buttons'>
-    <button className='btn1' onClick={handleincrement}>increment</button>
-    <button className='btn2' onClick={ handledecrement}>decrement</button>
-    <button  className='btn3' onClick={handlereset}>reset</button>
+  return (
+    <>
+    <ToastContainer/>
+    <div>
+      <button onClick={handlesubmit}>click for message</button>
     </div>
     </>
   )
-
 }
+
 export default App

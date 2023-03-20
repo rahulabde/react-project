4a02f24edfeb882ba5d14c11768d1f895576ceda
Rahul <rahulabde@gmail.com>
2023-03-20 22:38:38 +0530

    Refs in function based component eg:1

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,16 @@
-import { useRef } from "react"
-import React, { Component ,createRef} from 'react'
+import React ,{useRef}from 'react'
 
-export default class App extends Component {
-  constructor()
-  {
-    super()
-    this.state={company:"Q spiders"}
-    this.spanRef=createRef()
-    this.btnRef=createRef()
+const App = () => {
+  let h1Ref=useRef()
+  let handlechange =()=>{
+    h1Ref.current.style.color="red"
   }
-  render() {
-    return (
-      <> <h1>
-      <span ref={this.spanRef}>{this.state.company}</span> <br />
-      <button  ref={this.btnRef} onClick={()=>{
-        this.setState({company:"J spiders"})
-        this.spanRef.current.style.color="orange"
-        this.spanRef.current.style.border="2px"
-        this.spanRef.current.style.padding="2px"
-        this.spanRef.current.style.margin="2px"
-        this.btnRef.current.style.color="red"
-        this.btnRef.current.style.backgroundcoler="black"
-        this.btnRef.current.style.borderRadius="25%"
-      }}> change company</button>
-      </h1>
-      </>
-    )
-  }
+  return (
+    <>
+    <h1 ref={h1Ref}>app component</h1>
+    <button onClick={handlechange}>change</button>
+    </>
+  )
 }
+
+export default App

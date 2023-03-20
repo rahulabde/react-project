b884d9d843179ab3eb05834690e8acb908d1f332
Rahul <rahulabde@gmail.com>
2023-03-20 23:10:39 +0530

    Refs  eg:3

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,26 @@
-import React ,{useRef}from 'react'
+import React ,{useState,useRef}from 'react'
 
+import Img from "./car.jpg"
 const App = () => {
-  let h1Ref=useRef()
-  let handlechange =()=>{
-    h1Ref.current.style.color="red"
-  }
+
+  let [photo,setphoto]=useState(Img)
+  let [loading,setloading]=useState(true)
+  let imgRef=useRef()
+  let handlechange=()=>{
+    if(loading==true){
+      setloading(!true)
+    imgRef.current.style.borderRadius="25%"
+    imgRef.current.style.border="2px"
+     }else{
+      setloading(!false)
+      imgRef.current.style.borderRadius="0"
+    imgRef.current.style.border="none"
+     } }
   return (
-    <>
-    <h1 ref={h1Ref}>app component</h1>
-    <button onClick={handlechange}>change</button>
-    </>
+    <div>
+      <img src={Img} alt="spy" ref={imgRef } height="200px" width="200px" />
+      <button onClick={handlechange}> {loading ? "on":"off"}</button>
+    </div>
   )
 }
 

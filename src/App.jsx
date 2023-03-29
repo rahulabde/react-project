9c0dd7ff747a26063e03197ba2b944db023cf7ad
Rahul <rahulabde@gmail.com>
2023-03-29 11:56:41 +0530

    useEffect example1

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import Hoc from './Hoc'
+import React,{useState,useEffect} from 'react'
 
-const App = (props) => {
+const App = () => {
+    let[count,setcount]=useState(0)
+    useEffect(()=>{
+        document.title=`you clicked${count}times`} ,[count]
+    )
+       
+
   return (
-    <div>{props.data}</div>
+    <>
+    <h1>{count}</h1>
+    <button onClick={()=>{setcount(count+1)}}>increment</button>
+    </>
   )
 }
 
-export default Hoc(App)
+export default App
